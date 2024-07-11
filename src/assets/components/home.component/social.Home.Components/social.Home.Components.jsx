ab107c53e02c11ddb7f07077a7853963df802d35
Rahul Yadav <rahulcs989@gmail.com>
2024-07-11 23:55:07 +0530

    home page basic work Date:11-july-24

--- src/assets/components/home.component/social.Home.Components/social.Home.Components.jsx
+++ src/assets/components/home.component/social.Home.Components/social.Home.Components.jsx
@@ -16,22 +16,40 @@ export default function Social() {
         <div className="flex justify-center">
           <div className="grid grid-rows-1 grid-cols-6 gap-24 text-3xl">
             <a href="https://facebook.com" aria-label="Facebook">
-              <FontAwesomeIcon icon={faFacebookF} />
+              <FontAwesomeIcon
+                icon={faFacebookF}
+                className="text-gray-300 hover:text-blue-700 duration-300 transform hover:scale-125"
+              />
             </a>
             <a href="https://twitter.com" aria-label="Twitter">
-              <FontAwesomeIcon icon={faXTwitter} />
+              <FontAwesomeIcon
+                icon={faXTwitter}
+                className="text-gray-300 hover:text-black duration-300 transform hover:scale-125"
+              />
             </a>
             <a href="https://instagram.com" aria-label="Instagram">
-              <FontAwesomeIcon icon={faInstagram} />
+              <FontAwesomeIcon
+                icon={faInstagram}
+                className="text-gray-300 hover:text-pink-700 duration-300 transform hover:scale-125"
+              />
             </a>
             <a href="https://github.com" aria-label="Github">
-              <FontAwesomeIcon icon={faGithub} />
+              <FontAwesomeIcon
+                icon={faGithub}
+                className="text-gray-300 hover:text-black duration-300 transform hover:scale-125"
+              />
             </a>
             <a href="https://linkedin.com" aria-label="Linkedin">
-              <FontAwesomeIcon icon={faLinkedinIn} />
+              <FontAwesomeIcon
+                icon={faLinkedinIn}
+                className="text-gray-300 hover:text-blue-900 duration-300 transform hover:scale-125"
+              />
             </a>
             <a href="https://youtube.com" aria-label="YouTube">
-              <FontAwesomeIcon icon={faYoutube} />
+              <FontAwesomeIcon
+                icon={faYoutube}
+                className="text-gray-300 hover:text-red-700 duration-300 transform hover:scale-125"
+              />
             </a>
           </div>
         </div>
