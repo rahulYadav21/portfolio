4738e6896087944961b77b659b9c55098db35a06
Rahul Yadav <rahulcs989@gmail.com>
2024-08-07 19:41:10 +0530

    all stuff is done

--- src/assets/components/home.component/headText.Home.Component/headText.Home.Component.jsx
+++ src/assets/components/home.component/headText.Home.Component/headText.Home.Component.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import { ReactTyped } from "react-typed";
 import "../home.Component.css";
 export default function HeadText() {
+  const strings = [
+    "<span>an</span> <span class='text-purple-400'>Engineer</span>",
+    "<span>a</span> <span class='text-purple-400'>Coder</span>",
+    "<span>an</span> <span class='text-purple-400'>Developer</span>",
+    "<span>a</span> <span class='text-purple-400'>Innovator</span>",
+  ];
   return (
     <>
       <div className="text-center justify-center grid grid-rows-1 text-5xl pt-14 pb-10 lg:pt-20 lg:pb-10 2xl:pt-24 2xl:pb-24 lg:text-7xl md:text-7xl 2xl:text-8xl">
         <h1>
-          I'm a&nbsp;
+          <span>I'm </span>
           <ReactTyped
-            className="home-typed-text text-purple-400"
-            strings={["Engineer", "Coder", "Developer", "Innovator"]}
+            className="home-typed-text tracking-tight lg:tracking-normal 2xl:tracking-normal"
+            strings={strings}
             typeSpeed={100}
             backSpeed={100}
             loop
